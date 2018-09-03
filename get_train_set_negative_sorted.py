0b1e5eb27c0cfd5364efa226f62ea50e5135524e
PxLin <px.lin1@gmail.com>
2018-09-04 02:52:14 +1000

    modify test set

---
+++ get_train_set_negative_sorted.py
@@ -0,0 +1,81 @@
+import networkx as nx
+import numpy as np
+import timeit
+import priority_queue as pq
+
+time0 = timeit.default_timer()
+'''Read the pairs'''
+with np.load('filtered_data.npz') as fd:
+    pairs = fd['pairs']
+
+time1 = timeit.default_timer()
+print('Time for reading file: ', time1 - time0)
+
+'''Create a digraph for the task'''
+DG = nx.DiGraph()
+DG.add_edges_from(pairs)
+
+'''Create a undirected graph for computing AA, JC and RA'''
+UDG = nx.Graph()
+UDG.add_edges_from(pairs)
+
+time2 = timeit.default_timer()
+print('Time for creating graphs: ', time2 - time1)
+
+'''Get nodes edges, and non-edges'''
+non_edges = nx.non_edges(DG)
+candidates = pq()
+for ne in non_edges:
+    AA = nx.adamic_adar_index(UDG, [ne])
+    try:
+        for u, v, p in AA:
+            candidates.push((u,v), -p)
+    except ZeroDivisionError:
+        candidates.push((u, v), 0)
+        pass
+
+
+'''Compute HAA, HJC and HRA'''
+HAA = []
+HJC = []
+HRA = []
+
+for i in range(5000000):
+    e = candidates.pop()
+    AA = nx.adamic_adar_index(UDG, [e])
+    JC = nx.jaccard_coefficient(UDG, [e])
+    RA = nx.resource_allocation_index(UDG, [e])
+    SD = DG.in_degree(e[1]) - DG.in_degree(e[0])  # specificity_difference
+    if SD < 0:
+        HAA.append(0)
+        HJC.append(0)
+        HRA.append(0)
+    else:
+        try:
+            for u, v, p in AA:
+                HAA.append(p)
+        except ZeroDivisionError:
+                HAA.append(0)
+                pass
+
+        try:
+            for u, v, p in JC:
+                HJC.append(p)
+        except ZeroDivisionError:
+                HJC.append(0)
+                pass
+
+        try:
+            for u, v, p in RA:
+                HRA.append(p)
+        except ZeroDivisionError:
+                HRA.append(0)
+                pass
+
+
+time3 = timeit.default_timer()
+
+print('Time for calculating features: ', time3 - time2)
+
+'''Store the feature scores'''
+np.savez_compressed("train_features_negative", HAA=HAA, HJC=HJC, HRA=HRA)
