dec79ad8705670a7af5880deb9cf7a253ec449ac
hyesookim <Hyesoo@hyesukimui-MacBook-Air.local>
2018-09-05 19:49:40 +1000

    modified logistics

--- logistics.py
+++ logistics.py
@@ -1,4 +1,4 @@
-import numpy as np 
+import numpy as np
 
 # load the training data
 with np.load('features_positive.npz') as fp:
@@ -18,34 +18,41 @@ train_features = {'HAA': np.array(feature_HAA),
                   'HJC': np.array(feature_HJC),
                   'HRA': np.array(feature_HRA)}
 
-# np.column_stack(feature_HAA, feature_HJC, feature_HRA)
-X = np.matrix([feature_HAA, feature_HJC, feature_HRA]).T
-print(X.shape)
-
-# X.T = np.transpose(feature_HAA, feature_HJC, feature_HRA)
+train = np.matrix([feature_HAA, feature_HJC, feature_HRA]).T
 
 train_labels = np.transpose([1 for x in range(len(HAA_train_positive))]
                         + [0 for x in range(len(HAA_train_negative))])
 
-# print(train_labels.shape)
-# load the test data
-with np.load('test_features.npz') as tft:
-    HAA_test = tft['HAA']
-    HJC_test = tft['HJC']
-    HRA_test = tft['HRA']
-    
-test_features = {'HAA': np.array(HAA_test),
-                 'HJC': np.array(HJC_test),
-                 'HRA': np.array(HRA_test)} 
-
-Y = np.matrix([HAA_test, HJC_test, HRA_test]).T 
-print(Y.shape)
-
-# logsitic regression
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(train, train_labels)
 from sklearn.linear_model import LogisticRegression
 clf = LogisticRegression(C=1)
-clf.fit(X, train_labels)          
+clf.fit(X_train, Y_train)          
 
 from sklearn.metrics import accuracy_score
-test_pred = clf.predict(Y)
-accuracy_score(Y, test_pred.round(), normalize=False)
+Y_test_pred = clf.predict(X_test)
+print(accuracy_score(Y_test, Y_test_pred))
+
+
+
+# load the test data
+
+#with np.load('test_features.npz') as tft:
+#    HAA_test = tft['HAA']
+#    HJC_test = tft['HJC']
+#    HRA_test = tft['HRA']
+    
+#test_features = {'HAA': np.array(HAA_test),
+#                 'HJC': np.array(HJC_test),
+#                 'HRA': np.array(HRA_test)} 
+
+#test = np.matrix([HAA_test, HJC_test, HRA_test]).T 
+
+# print (test)
+# print(test.shape)
+
+# a = np.array(test)
+# b = a.ravel()
+# print(b)
+# print(len(b))
+# logsitic regression
